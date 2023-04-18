45f134d90c2342cb94b70cb14f2af285016cc685
wangfei <wangfeia@uniontech.com>
2023-04-18 10:49:01 +0800

    Auto-commit: 2023年 04月 18日 星期二 10:49:01 CST

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+LOG_FILE="/home/fei/auto-commit.log"
 MESSAGE="Auto-commit: $(date)"
 REPO_PATH="/home/fei/repo/job-data"
+echo "$(date): auto commit" >> "$LOG_FILE"
 git -C "$REPO_PATH" add -A
 git -C "$REPO_PATH" commit -m "$MESSAGE"
 git -C "$REPO_PATH" push origin main -f
