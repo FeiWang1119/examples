e678ca59a2b70d7f7eaa9f72e8326e5aae9b6d05
wangfei <wangfeia@uniontech.com>
2023-04-18 14:27:14 +0800

    Auto-commit: 2023年 04月 18日 星期二 14:27:14 CST

--- auto-commit.sh
+++ auto-commit.sh
@@ -3,7 +3,7 @@
 LOG_FILE="/home/fei/auto-commit.log"
 MESSAGE="Auto-commit: $(date)"
 REPO_PATH="/home/fei/repo/job-data"
-echo "$(date): auto commit" >> "$LOG_FILE"
-git -C "$REPO_PATH" add -A 2>>"$LOG_FILE"
-git -C "$REPO_PATH" commit -m "$MESSAGE" 2>>"$LOG_FILE"
-git -C "$REPO_PATH" push origin main -f 2>>"$LOG_FILE"
+echo "$(date): auto commit" >> $LOG_FILE
+git -C "$REPO_PATH" add -A 2>>$LOG_FILE
+git -C "$REPO_PATH" commit -m "$MESSAGE" 2>>$LOG_FILE
+git -C "$REPO_PATH" push origin main -f 2>>$LOG_FILE
--- clone-dtk.sh
+++ clone-dtk.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+git clone git@github.com:FeiWang1119/dtkcore.git $1/dtkcore
+cd $1/dtkcore
+git remote add linuxdeepin git@github.com:linuxdeepin/dtkcore.git
 
-git clone git@github.com:FeiWang1119/dtkwidget.git $1
-git clone git@github.com:FeiWang1119/dtkcore.git $1
-git clone git@github.com:FeiWang1119/dtkgui.git $1
-git clone git@github.com:FeiWang1119/qt5integration.git $1
-git clone git@github.com:FeiWang1119/qt5platform-plugins.git $1
+git clone git@github.com:FeiWang1119/dtkgui.git $1/dtkgui
+cd $1/dtkgui
+git remote add linuxdeepin git@github.com:linuxdeepin/dtkgui.git
+
+git clone git@github.com:FeiWang1119/dtkwidget.git $1/dtkwidget
+cd $1/dtkwidget
+git remote add linuxdeepin git@github.com:linuxdeepin/dtkwidget.git
+
+git clone git@github.com:FeiWang1119/qt5integration.git $1/qt5integration
+cd $1/qt5integration
+git remote add linuxdeepin git@github.com:linuxdeepin/qt5integration.git
+
+git clone git@github.com:FeiWang1119/qt5platform-plugins.git $1/qt5platform-plugins
+cd $1/qt5platform-plugins
+git remote add linuxdeepin git@github.com:linuxdeepin/qt5platform-plugins.git
 
 
