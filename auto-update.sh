32438769e45cb65834540621f1078b9e9b7e9ed7
wangfei <wangfeia@uniontech.com>
2023-04-18 11:13:01 +0800

    Auto-commit: 2023年 04月 18日 星期二 11:13:01 CST

--- auto-update.sh
+++ auto-update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 LOG_FILE="/home/fei/auto-update.log"
 REPO_PATH="/home/fei/repo/dtkwidget"
 git -C "$1" pull linuxdeepin master:master --rebase
-echo "$(date): auto commit" >> "$LOG_FILE"
+echo "$(date): auto update $1" >> "$LOG_FILE"
