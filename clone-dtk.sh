50fbc1bf735b8dd6a2a5f3b76693c2002c67fbcc
wangfei <wangfeia@uniontech.com>
2023-04-18 14:00:01 +0800

    Auto-commit: 2023年 04月 18日 星期二 14:00:01 CST

---
+++ clone-dtk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+git clone git@github.com:FeiWang1119/dtkwidget.git $1
+git clone git@github.com:FeiWang1119/dtkcore.git $1
+git clone git@github.com:FeiWang1119/dtkgui.git $1
+git clone git@github.com:FeiWang1119/qt5integration.git $1
+git clone git@github.com:FeiWang1119/qt5platform-plugins.git $1
+
+
