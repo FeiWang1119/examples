1c0a4a17b7e219083939541c7c4002d6ed9e49b8
wangfei <wangfeia@uniontech.com>
2023-04-19 18:00:01 +0800

    Auto-commit: 2023年 04月 19日 星期三 18:00:01 CST

--- scripts/clone-dtk.sh
+++ scripts/clone-dtk.sh
@@ -2,65 +2,24 @@
 
 # Usage: clone-dtk.sh ./repo
 
-INIT_PATH="$(pwd)"
-echo "Current path: $INIT_PATH"
+init_path="$(pwd)"
+echo "Current path: $init_path"
 
-DTKCORE=""
-DTKGUI=""
-DTKWIDGET=""
-QI=""
-QP=""
+repos="dtkcore dtkgui dtkwidget qt5integration  qt5platform-plugins"
 
-DTKCORE_PATH=""
-DTKGUI_PATH=""
-DTKWIDGET_PATH=""
-QI_PATH=""
-QP_PATH=""
-
-if [ -z "$1" ]; 
-then
-    DTKCORE_PATH="./dtkcore"
-    DTKGUI_PATH="./dtkgui"
-    DTKWIDGET_PATH="./dtkwidget"
-    QI_PATH="./qt5integration"
-    QP_PATH="./qt5platform-plugins"
-    echo "clone to  current directory"
-else
-    DTKCORE="$1/dtkcore"
-    DTKGUI="$1/dtkgui"
-    DTKWIDGET="$1/dtkwidget"
-    QI="$1/qt5integration"
-    QP="$1/qt5platform-plugins"
-
-    DTKCORE_PATH=$DTKCORE
-    DTKGUI_PATH=$DTKGUI
-    DTKWIDGET_PATH=$DTKWIDGET
-    QI_PATH=$QI
-    QP_PATH=$QP
-fi
-
-git clone git@github.com:FeiWang1119/dtkcore.git $DTKCORE
-cd $DTKCORE_PATH
-git remote add linuxdeepin git@github.com:linuxdeepin/dtkcore.git
-cd $INIT_PATH
-
-git clone git@github.com:FeiWang1119/dtkgui.git $DTKGUI
-cd $DTKGUI_PATH
-git remote add linuxdeepin git@github.com:linuxdeepin/dtkgui.git
-cd $INIT_PATH
-
-git clone git@github.com:FeiWang1119/dtkwidget.git $DTKWIDGET
-cd $DTKWIDGET_PATH
-git remote add linuxdeepin git@github.com:linuxdeepin/dtkwidget.git
-cd $INIT_PATH
-
-git clone git@github.com:FeiWang1119/qt5integration.git $QI
-cd $QI_PATH
-git remote add linuxdeepin git@github.com:linuxdeepin/qt5integration.git
-cd $INIT_PATH
-
-git clone git@github.com:FeiWang1119/qt5platform-plugins.git $QP
-cd $QP_PATH
-git remote add linuxdeepin git@github.com:linuxdeepin/qt5platform-plugins.git
-cd $INIT_PATH
-
+for repo in $repos
+do
+  current_path=""
+  repo_path=""
+  if [ -z "$1" ]; 
+  then
+    repo_path="./$repo"
+  else
+    current_path="$1/$repo"
+    repo_path=$current_path
+  fi
+  git clone git@github.com:FeiWang1119/$repo.git $current_path
+  cd $repo_path
+  git remote add linuxdeepin git@github.com:linuxdeepin/$repo.git
+  cd $init_path
+done
