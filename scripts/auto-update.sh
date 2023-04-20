8b2fd8c7b94670198bdf0accb7f078934a45c8e9
wangfei <wangfeia@uniontech.com>
2023-04-20 11:00:01 +0800

    Auto-commit: 2023年 04月 20日 星期四 11:00:01 CST

--- scripts/auto-update.sh
+++ scripts/auto-update.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-LOG_FILE="/home/fei/auto-update.log"
-REPO_PATH="/home/fei/repo/dtkwidget"
+# Usage: auto-update.sh $HOME/repo/dtkwidget
 
-echo "$(date): auto update $1" >> $LOG_FILE
-git -C "$1" pull linuxdeepin master:master --rebase 2>>$LOG_FILE
+if [ $# -lt 1 ]; then
+  echo "Usage: $0  repoDir"
+  exit 1
+fi
+
+log_file="/home/fei/auto-update.log"
+
+echo "$(date): auto update $1" >> $log_file
+git -C "$1" pull linuxdeepin master:master --rebase 2>>$log_file
+if [ $? -eq 0 ]; then
+  echo "update success!"  >> $log_file
+else
+  echo "first update fail. try to stash changes and pull again!" >> $log_file
+  git -C "$1" stash  
+  git -C "$1" pull linuxdeepin master:master --rebase 2>>$log_file
+  if [ $? -eq 0 ]; then
+    echo "update success!"  >> $log_file
+  else 
+    echo "update fail!"  >> $log_file
+   fi
+  git -C "$1" stash pop
+fi
