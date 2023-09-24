027c3f7fd6caadaa6b06282c72432327448ba52f
wangfei <wangfeia@uniontech.com>
2023-09-24 12:59:51 +0800

    add colorscheme

---
+++ configs/plugins/colorscheme.lua
@@ -0,0 +1,53 @@
+return {
+  -- gruvbox
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
+
+  -- tokyonight theme
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = true,
+  --   opts = { style = "night" }, -- storm, night, moon, day
+  -- },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      term_colors = true,
+      transparent_background = true,
+      styles = {
+        comments = {},
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
+  },
+}
