e6a1ae1d473aa93e6fd67180f847596f3b5efcdb
wangfei <wangfei@deepin.org>
2023-11-10 16:27:38 +0800

    update neovim

--- configs/plugins/bookmarks.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "tomasky/bookmarks.nvim",
-  event = "VimEnter",
-  config = function()
-    require("bookmarks").setup()
-    require("telescope").load_extension("bookmarks")
-  end,
-}
