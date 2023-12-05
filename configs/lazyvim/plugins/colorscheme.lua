df8daf02da9957c75e73c13dc48427f5a2fb8e79
wangfei <wangfei@deepin.org>
2023-12-05 14:01:17 +0800

    update colortheme

--- configs/lazyvim/plugins/colorscheme.lua
+++ configs/lazyvim/plugins/colorscheme.lua
@@ -1,77 +1,9 @@
 return {
-  -- gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- tokyonight theme
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "night" }, -- storm, night, moon, day
-  },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
-
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    opts = {
-      term_colors = true,
-      transparent_background = false,
-      styles = {
-        comments = {},
-        conditionals = {},
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-      },
-      color_overrides = {
-        mocha = {
-          base = "#313131",
-          mantle = "#313131",
-          crust = "#313131",
-        },
-      },
-      integrations = {
-        alpha = true,
-        cmp = true,
-        flash = true,
-        gitsigns = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        lsp_trouble = true,
-        mason = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        noice = true,
-        notify = true,
-        neotree = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        which_key = true,
-      },
-    },
-  },
+	{ "ellisonleao/gruvbox.nvim" },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "gruvbox",
+		},
+	},
 }
