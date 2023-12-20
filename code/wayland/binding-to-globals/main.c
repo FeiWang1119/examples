71a6db4b8fd86d1ed370c542a2e00567d1830a11
wangfei <wangfei@deepin.org>
2023-12-20 13:36:01 +0800

    update binding-to-globals

--- code/wayland/binding-to-globals/main.c
+++ code/wayland/binding-to-globals/main.c
@@ -2,13 +2,26 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+
+struct our_state
+{
+    // ...
+    struct wl_compositor *compositor;
+    // ...
+};
 
 static void registry_handle_global(void *data,
-                                   struct wl_registry *registry,
+                                   struct wl_registry *wl_registry,
                                    uint32_t name,
                                    const char *interface,
                                    uint32_t version)
 {
+    struct our_state *state = data;
+    if (strcmp(interface, wl_compositor_interface.name) == 0) {
+        state->compositor = wl_registry_bind(wl_registry, name, &wl_compositor_interface, 4);
+    }
+
     printf("interface: '%s', version: %d, name: %d\n", interface, version, name);
 }
 
@@ -24,6 +37,7 @@ static const struct wl_registry_listener registry_listener = {
 
 int main(int argc, char *argv[])
 {
+    struct our_state state = { 0 };
     struct wl_display *display = wl_display_connect(NULL);
     struct wl_registry *registry = wl_display_get_registry(display);
     wl_registry_add_listener(registry, &registry_listener, NULL);
