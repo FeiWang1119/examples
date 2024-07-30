c7a295114de83918b0a13cfadba6e4da69555f4a
wangfei <wangfei@deepin.org>
2024-07-30 10:49:18 +0800

    add tvbox config

--- configs/lazyvim/lua/plugins/session.lua
+++ configs/lazyvim/lua/plugins/session.lua
@@ -12,6 +12,11 @@ return {
       vim.keymap.set("n", "<leader>ls", require("auto-session.session-lens").search_session, {
         noremap = true,
       }),
+      auto_session_enable_last_session = true,
+      auto_session_use_git_branch = true,
+      auto_session_enabled = true,
+      auto_save_enabled = true,
+      auto_restore_enabled = false,
     })
   end,
 }
