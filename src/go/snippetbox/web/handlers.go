82324df36f781cb2c874dd35f4fef8804ac8981d
wangfei <wangfei@deepin.org>
2024-09-14 16:15:27 +0800

    add the helpers and apply it

--- src/go/snippetbox/web/handlers.go
+++ src/go/snippetbox/web/handlers.go
@@ -11,7 +11,7 @@ import (
 // *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.noFound(w) // Use the noFound() helper.
 		return
 	}
 
@@ -30,8 +30,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// Notice that we can pass the slice of file paths as a variadic parameter.
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serveError(w, err) // Use the serveError() helper.
 		return
 	}
 
@@ -40,8 +39,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// we want to pass in, which for now we'll leave as nil.
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serveError(w, err) // Use the serveError() helper.
 	}
 }
 
@@ -50,7 +48,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound() helper.
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
@@ -61,7 +59,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method not allowed", 405)
+		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper.
 		return
 	}
 
